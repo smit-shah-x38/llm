871ed4e4aae65818f3088f52b45c4c5b30776b73
= <=>
2023-08-14 08:56:51 +0000

    .....

--- historian.py
+++ historian.py
@@ -1,5 +1,7 @@
 from transformers import pipeline
 
 prompt = "Hugging Face is a community-based open-source platform for machine learning."
-generator = pipeline(task="text-generation")
+generator = pipeline(
+    task="text-generation", model="/var/wd_smit/localdata/models/Llama-2-7b-chat-hf"
+)
 print(generator(prompt))  # doctest: +SKIP
--- historymakeragain.py
+++ historymakeragain.py
@@ -27,7 +27,7 @@ llm = OpenAI(openai_api_key="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
 
 # import wikipedia sentences
 candidate_sentences = pd.read_csv(
-    "/var/basefolder_smit/largelm/localdata/input_data.txt-out.csv"
+    "/var/wd_smit/external_repos/knowledge-graph/data/output/kg/input_data.txt-out.csv"
 )
 
 df = pd.DataFrame(
