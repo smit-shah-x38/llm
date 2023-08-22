2520f86b30c77c832c8d813ed17cd5f7b44852d9
= <=>
2023-08-22 13:21:17 +0000

    working script

--- word2vecinternal.py
+++ word2vecinternal.py
@@ -11,76 +11,6 @@ from tensorflow.keras import layers
 SEED = 42
 AUTOTUNE = tf.data.AUTOTUNE
 
-sentence = "The wide road shimmered in the hot sun"
-tokens = list(sentence.lower().split())
-print(len(tokens))
-
-vocab, index = {}, 1  # start indexing from 1
-vocab["<pad>"] = 0  # add a padding token
-for token in tokens:
-    if token not in vocab:
-        vocab[token] = index
-        index += 1
-vocab_size = len(vocab)
-print(vocab)
-
-inverse_vocab = {index: token for token, index in vocab.items()}
-print(inverse_vocab)
-example_sequence = [vocab[word] for word in tokens]
-print(example_sequence)
-
-window_size = 2
-positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
-    example_sequence,
-    vocabulary_size=vocab_size,
-    window_size=window_size,
-    negative_samples=0,
-)
-print(len(positive_skip_grams))
-
-for target, context in positive_skip_grams[:5]:
-    print(f"({target}, {context}): ({inverse_vocab[target]}, {inverse_vocab[context]})")
-
-target_word, context_word = positive_skip_grams[0]
-
-num_ns = 4
-
-context_class = tf.reshape(tf.constant(context_word, dtype="int64"), (1, 1))
-negative_sampling_candidates, _, _ = tf.random.log_uniform_candidate_sampler(
-    true_classes=context_class,  # class that should be sampled as 'positive'
-    num_true=1,  # each positive skip-gram has 1 positive context class
-    num_sampled=num_ns,  # number of negative context words to sample
-    unique=True,  # all the negative samples should be unique
-    range_max=vocab_size,  # pick index of the samples from [0, vocab_size]
-    seed=SEED,  # seed for reproducibility
-    name="negative_sampling",  # name of this operation
-)
-print(negative_sampling_candidates)
-print([inverse_vocab[index.numpy()] for index in negative_sampling_candidates])
-
-squeezed_context_class = tf.squeeze(context_class, 1)
-
-# Concatenate a positive context word with negative sampled words.
-context = tf.concat([squeezed_context_class, negative_sampling_candidates], 0)
-
-# Label the first context word as `1` (positive) followed by `num_ns` `0`s (negative).
-label = tf.constant([1] + [0] * num_ns, dtype="int64")
-target = target_word
-
-
-print(f"target_index    : {target}")
-print(f"target_word     : {inverse_vocab[target_word]}")
-print(f"context_indices : {context}")
-print(f"context_words   : {[inverse_vocab[c.numpy()] for c in context]}")
-print(f"label           : {label}")
-
-print("target  :", target)
-print("context :", context)
-print("label   :", label)
-
-sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(size=10)
-print(sampling_table)
-
 
 def generate_training_data(sequences, window_size, num_ns, vocab_size, seed):
     # Elements of each training example are appended to these lists.
@@ -175,7 +105,8 @@ print(inverse_vocab[:20])
 
 text_vector_ds = text_ds.batch(1024).prefetch(AUTOTUNE).map(vectorize_layer).unbatch()
 
-sequences = list(text_vector_ds.as_numpy_iterator())
+
+sequences = list(text_vector_ds)
 print(len(sequences))
 
 for seq in sequences[:5]:
---
+++ word2vecinternal2.py
@@ -0,0 +1,52 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+import warnings
+
+import nltk
+
+nltk.download("punkt")
+
+warnings.filterwarnings(action="ignore")
+import gensim
+from gensim.models import Word2Vec
+import tensorflow as tf
+
+# Load your text data
+# sample = open("your_text_file.txt", "r")
+
+import requests
+
+url = "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt"
+response = requests.get(url)
+s = response.text
+
+# s = tf.keras.utils.get_file(
+#     "shakespeare.txt",
+#     "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt",
+# )
+
+s = s.replace("\n", " ")
+
+# with open(s) as f:
+#     lines = f.read().splitlines()
+# for line in lines[:20]:
+#     print(line)
+
+# s = sample.read()
+# f = s.replace("\n", " ")
+
+# Tokenize your text data
+data = []
+for i in sent_tokenize(s):
+    temp = []
+    for j in word_tokenize(i):
+        temp.append(j.lower())
+    data.append(temp)
+
+# Train the word2vec model
+model = gensim.models.Word2Vec(data, min_count=1, vector_size=100, window=5)
+
+# Test the model
+print(
+    "Cosine similarity between 'alonso' and 'antonio': ",
+    model.wv.similarity("alonso", "antonio"),
+)
